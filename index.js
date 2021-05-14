dd56b3e3150696362ce6c33d0685872a1f29bb2f
Laure-Anais Giowachini <giowachini.la@gmail.com>
2021-05-14 15:06:53 +0200

    Mentorat 2 - JS

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ fetch('http://localhost:3000/api/teddies')
   .then(response => response.json())
   .then(data =>
     {
+      // creation  gallery pour stocker les infos de l'API / ours
+        const myGallery = document.getElementById('content')
+        let domNom
         console.log(data)
+        for(let i=0; i <data.length; i++)
+        {
+          console.log(data[i].name)
+          console.log(data[i].price)
+          domNom = document.createElement("h2")
+          domNom.textContent = data[i].name
+          myGallery.appendChild(domNom)
+        }
     });
 
---
+++ produits.js
@@ -0,0 +1,7 @@
+fetch('http://localhost:3000/api/teddies')
+then(response => response.json(){
+    if ()
+        return
+})
+
+// utiliser Innerhtml pour modifier les oursons 
