e2b1650f0b27d051e806763e452406ca341636a1
Laure-Anais Giowachini <giowachini.la@gmail.com>
2021-05-19 22:13:03 +0200

    Essai index.htmls

--- index.js
+++ index.js
@@ -12,12 +12,47 @@ fetch('http://localhost:3000/api/teddies')
           domNom = document.createElement("h3")
           domNom.textContent = data[i].name
           myGallery.appendChild(domNom)
+        }    
         
-  
-          domNom = document.createElement("h4")
-          domNom.textContent = data[i].price
-          myGallery.appendChild(domNom)
-          console.log(data[i].price)
-        }
-    });
+        let domPrice
+        console.log(data)
+        for(let i=0; i <data.length; i++)
+          {  console.log(data[i].price)
+            domPrice = document.createElement("h4")
+            domPrice.textContent = data[i].price
+            myGallery.appendChild(domPrice)
+          }
 
+        let domImage
+        console.log(data)
+        for(let i=0; i <data.length; i++)
+          {  console.log(data[i].imageUrl)
+             domImage = document.createElement("h4")
+             domImage.textContent = data[i].imageUrl
+             myGallery.appendChild(domImage)
+          } 
+         
+   });
+
+
+    
+/*
+Pourquoi sur le webinar il met le html dans le js?? 
+Comment mettre en page les articles 
+Avoir 
+titre
+prix 
+bouton -> selectionner votre oursons
+
+document.querySelector('content').innerHTML += `<div id="content">
+        <div class="article_teddy">$(article.teddy)
+            <h3 class="name_teddy">$(name.teddy)<h3>
+            <p class="price_teddy">$(price.teddy)</p>
+            <span class="image_teddy"></span>
+            <button class="btn"></button>
+        </div>
+    </div>`
+  
+     
+                  
+*/
